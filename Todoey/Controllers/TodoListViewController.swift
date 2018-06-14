deea5174ebc4eae63630a46d6b59bd81808862ef
Gina Sprint <sprint@sprint-macbook.local>
2018-06-14 13:38:33 -0700

    Convert to MVC design pattern w/add bug

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = [String]()
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
 
@@ -21,11 +21,23 @@ class TodoListViewController: UITableViewController {
         // since we inherit from UITableViewController, we don't need to set the data source or delegate
         // don't need to set up an IBOutlets
     
-        // check to see if TodolistArray is in there
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
         
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy"
+        itemArray.append(newItem3)
+        
+        // check to see if TodolistArray is in there
+//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+//            itemArray = items
+//        }
+//
         
     }
 
@@ -37,9 +49,15 @@ class TodoListViewController: UITableViewController {
     //MARK Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // there is a local and global tableView variable, refer to the same tableView can use either one
+        print("In tableView cellForAt:")
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
         // where do we specify the class of ToDoItemCell
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        // ternary in swift
+        // value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done == true ? .checkmark : .none
         
         return cell
     }
@@ -52,13 +70,11 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row], "clicked!")
         
-        let cell : UITableViewCell? = tableView.cellForRow(at: indexPath)
-        if cell?.accessoryType == .checkmark {
-            cell?.accessoryType = .none
-        }
-        else {
-            cell?.accessoryType = .checkmark
-        }
+        //let cell : UITableViewCell? = tableView.cellForRow(at: indexPath)
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        // forces tableView to call its datasource method again to reload data that is inside
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -86,7 +102,9 @@ class TodoListViewController: UITableViewController {
             //print(alert.textFields![0].text!)
             let text = textField.text!
             // should add checking code so that the text
-            self.itemArray.append(text)
+            let newItem = Item()
+            newItem.title = text
+            self.itemArray.append(newItem)
             // force update of table view
             self.tableView.reloadData()
             
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Gina Sprint on 6/14/18.
+//  Copyright © 2018 Gina Sprint. All rights reserved.
+//
+
+import Foundation
+
+// don't have to name Item to match name of file but is good practice
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+    
+    
+}
