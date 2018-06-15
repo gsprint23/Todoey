6c11a8478475ee16d6edddc51ea0c95c9616dd06
Gina Sprint <sprint@sprint-macbook.local>
2018-06-14 17:18:04 -0700

    Finish basic CRUD capabilities

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import CoreData // to get Item which is core data managed
 
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
-     // file path to documents folder
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
         // this is an array so grab first item
-        print(dataFilePath!)
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
         // since we inherit from UITableViewController, we don't need to set the data source or delegate
         // don't need to set up an IBOutlets
@@ -37,7 +37,7 @@ class TodoListViewController: UITableViewController {
     //MARK Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // there is a local and global tableView variable, refer to the same tableView can use either one
-        print("In tableView cellForAt:")
+        print("In tableView cellForAt")
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         let item = itemArray[indexPath.row]
         // where do we specify the class of ToDoItemCell
@@ -61,6 +61,9 @@ class TodoListViewController: UITableViewController {
         //let cell : UITableViewCell? = tableView.cellForRow(at: indexPath)
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        // remove instead of set check box
+//        let removed = itemArray.remove(at: indexPath.row)
+//        context.delete(removed)
         self.saveData()
         
         // forces tableView to call its datasource method again to reload data that is inside
@@ -92,8 +95,13 @@ class TodoListViewController: UITableViewController {
             //print(alert.textFields![0].text!)
             let text = textField.text!
             // should add checking code so that the text
-            let newItem = Item()
+            
+            // need to get AppDelegate object
+            // UIApplication.shared is a singleton, which is current app object
+            // cast UIApplicationDelegate? into AppDelegate
+            let newItem = Item(context: self.context)
             newItem.title = text
+            newItem.done = false
             self.itemArray.append(newItem)
             // force update of table view
             self.tableView.reloadData()
@@ -118,28 +126,23 @@ class TodoListViewController: UITableViewController {
     
 
     func saveData() {
-        let encoder = PropertyListEncoder()
+        // commit context to permanent storage
         do {
-            let data = try encoder.encode(itemArray)
-            // generic parameter Value cannot be inferred, need to declare Item.swift to implement Encodable Protocol
-            try data.write(to : dataFilePath!)
+           try context.save()
         } catch {
-            print("Error encoding item array, \(error)") // where is error defined?
+            print("Error saving context \(error)")
         }
     }
     
     func loadData() {
-        // try? turns result into an optional
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                // [Item].self refers to the type of array of Item, not an object of type [Item]
-                // TODO: follow up on [Item].self
-                itemArray = try decoder.decode([Item].self, from: data)
-            }
-            catch {
-                print("Error decoding item array, \(error)")
-            }
+        // few cases in swift when you need to specify a data type
+        // specify entity you are trying to request
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        }
+        catch {
+            print("Error fetching data from context \(error)")
         }
     }
 }
